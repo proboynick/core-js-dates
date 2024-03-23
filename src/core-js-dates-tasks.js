d75a45c6267ba088d674bd4fed3f06c7eb975316
Antony <kuzmi495@mail.ru>
2024-03-23 23:10:47 +0300

    fix: attempt to resolve failing test

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -252,28 +252,27 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  // const startDate = new Date(period.start.split('-').reverse().join('-'));
-  // const endDate = new Date(period.end.split('-').reverse().join('-'));
-  // let day = startDate.getDate();
-  // const startMonth = startDate.getMonth();
-  // const startYear = startDate.getFullYear();
-  // let currentDate = new Date(startYear, startMonth, day);
-  // const shedule = [];
-  // while (currentDate < endDate) {
-  //   for (let i = 0; i < countWorkDays; i += 1) {
-  //     currentDate = new Date(startYear, startMonth, day);
-  //     if (currentDate < endDate) {
-  //       shedule.push(
-  //         currentDate.toLocaleDateString('ru-RU').split('.').join('-')
-  //       );
-  //     }
-  //     day += 1;
-  //   }
-  //   day += countOffDays;
-  // }
-  // return shedule;
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const startDate = new Date(period.start.split('-').reverse().join('/'));
+  const endDate = new Date(period.end.split('-').reverse().join('/'));
+  let day = startDate.getDate();
+  const startMonth = startDate.getMonth();
+  const startYear = startDate.getFullYear();
+  let currentDate = new Date(startYear, startMonth, day);
+  const shedule = [];
+  while (currentDate <= endDate) {
+    for (let i = 0; i < countWorkDays; i += 1) {
+      currentDate = new Date(startYear, startMonth, day);
+      if (currentDate <= endDate) {
+        shedule.push(
+          currentDate.toLocaleDateString('ru-RU').split('.').join('-')
+        );
+      }
+      day += 1;
+    }
+    day += countOffDays;
+  }
+  return shedule;
 }
 
 /**
